58d4ac016f5ef0e09866b00fcd53658374d0fe5a
Alex Burka <durka42@gmail.com>
2013-05-15 21:14:30 -0500

    keyboard repeat and getchar-ish api

--- launchpad_ii/key.h
+++ launchpad_ii/key.h
@@ -3,16 +3,20 @@
 
 #include "common.h"
 
+#define KEYBUF 50
+#define EOF -1
+
 typedef struct
 {
     Port port;
     Pin pin;
 
-    unsigned short val;
-    char key;
+    unsigned short i, j;
+    char key[KEYBUF];
 } Key;
 
 void key(Key *this, Port port, Pin pin);
+int key_get(Key *this);
 
 #endif // _BURKA_KEY_H_
 
